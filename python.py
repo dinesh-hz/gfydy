2eb4cec9ede17c5ce3562a42cf9371e5301a736f
dinesh-hz <dineshmeij@gmail.com>
2023-07-29 08:31:23 +0530

    firstcommit

---
+++ python.py
@@ -0,0 +1,67 @@
+print("dinesh")
+
+Name = "dinesh" #string value
+age = 45       #int value
+email = "dineshmeij@gmail.com"   #string value
+salary = 145.25   #float
+
+
+# tHow to check varible  data type  sytex
+
+print(type(Name),Name)
+
+print(type(age)) 
+
+print(type(email))  
+
+print(type(salary))
+
+
+
+# this is how to check the varile id location
+
+print(id(Name))   
+
+
+#how to possible many varible strone in single line
+
+name,ages,gmaail,salery = ("dinsh",45,"dinesh@gmail.com",452.36)
+
+print(name,ages,gmaail,salery)
+
+
+
+#how to save list files sytexs
+
+stuendent_name = ["dinesh", "dimnesmeij", "dinehsmuru",45,4562.25]
+
+print("stuendent_name  of list",stuendent_name)
+
+
+nickname = 1
+
+
+nickname = 45
+
+
+print(nickname)
+
+#how to save set files sytexs
+
+hai = {"dinesh" , "muru" , "meij"}
+print("hai of set value" ,hai)
+
+#how to save dictery files sytexs
+
+
+cuctmer_id = {"Name" : "Dinesh", "Email" : "dineshmeij@gmail.com" , "Phone number" : 6384084141 , "sale" : 4525.256}
+
+print(cuctmer_id)
+
+#how to save frozenset files sytexs
+
+h = frozenset({"dinesh" , "dinesheij" , "outlook"})
+
+
+print(h)
+
